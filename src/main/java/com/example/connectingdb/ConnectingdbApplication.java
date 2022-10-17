47206f2aa9ffca3dd7fde0d51fe6eecd78c3fdbb
aulianar <arisky540@gmail.com>
2022-10-17 23:47:38 +0700

    add database

---
+++ src/main/java/com/example/connectingdb/ConnectingdbApplication.java
@@ -0,0 +1,13 @@
+package com.example.connectingdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectingdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectingdbApplication.class, args);
+	}
+
+}
